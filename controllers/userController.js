6f1d2ad4a7657b107a116218903aa2cba995dca3
Olena Liekan <olena.liekan@gmail.com>
2025-01-02 18:47:53 +0200

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,7 +119,6 @@ class UserController {
       where: {},
       include: [
         { model: UserOrder, as: 'order', include: [{ model: OrderItem, as: 'item' }] },
-        { model: UserPromocode, as: 'user_promocode' },
         {
           model: UserAddress,
           as: 'address',
