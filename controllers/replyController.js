ace3947fc7c1154a0e54e5b4458ce349a4ef5435
Olena Liekan <olena.liekan@gmail.com>
2025-08-20 18:11:58 +0300

    Feature: Added response update functionality

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -16,6 +16,22 @@ class ReplyController {
     const replies = await Reply.findAll();
     return res.json(replies);
   }
+
+  async update(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { text } = req.body;
+      const options = { where: { id: id } };
+      let props = {};
+      if (text) {
+        props = { ...props, text };
+      }
+      const reply = await Reply.update(props, options);
+      return res.json(reply);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
 }
 
 module.exports = new ReplyController();
--- routes/replyRouter.js
+++ routes/replyRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const replyController = require('../controllers/replyController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/', replyController.create);
+router.post('/', checkRole('ADMIN'), replyController.create);
 router.get('/', replyController.getAll);
+router.patch('/:id', checkRole('ADMIN'), replyController.update);
 
 module.exports = router;
